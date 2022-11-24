11cd379335d114c75407d4cfaaaaa336087dfc88
M Zulfiansyah Bayu Pratama <16521512@std.stei.itb.ac.id>
2022-11-24 18:31:32 +0700

    make main

---
+++ src/main/main.py
@@ -0,0 +1,10 @@
+import sys, argparse
+from proccess.token import create_token
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type = argparse.FileType('r'))
+    args = parser.parse_args()
+    
+    token = create_token(args.file.name)
+    
