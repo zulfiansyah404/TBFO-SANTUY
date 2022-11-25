a39ec707f27c7d94bdb4d6571f5c25ae7b8ee4da
M Zulfiansyah Bayu Pratama <16521512@std.stei.itb.ac.id>
2022-11-25 11:09:44 +0700

    UPDATE CFG

--- src/examples/sample_04.js
+++ src/examples/sample_04.js
@@ -4,6 +4,9 @@ if (x > 0) {
 } else
 if (x == 0) {
     x = 0;
-} else {
+} else
+if (x < 0) {
     x = -1;
+} else {
+    x = 2;
 }
--- src/examples/sample_05.js
+++ src/examples/sample_05.js
@@ -1,5 +1,10 @@
 function cekKondisi(x) {
-    if (x > 1) {
-        re
+    if (x > 0) {
+        x = 1;
+    } else
+    if (x == 0) {
+        x = 0;
+    } else {
+        x = -1;
     }
 }
